c94782c76c565564e640408af365315e9f74700c
Gianluca De Prisco <mistergdp@gmail.com>
2022-02-25 18:16:46 +0100

    Updated home view
    Added bootstrap + static image

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from portfolio_app import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
